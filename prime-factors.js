3d7101cca001474547d0b98aa12796d78b4660db
Kevin Glad <kglad@wbteam.net>
2019-08-23 09:20:56 -0600

    Adding comments to prime-factors.js

--- prime-factors.js
+++ prime-factors.js
@@ -3,40 +3,65 @@
 // convenience to get you started writing code faster.
 //
 
+// Main function
 export function primeFactors (num) {
-  let factors = [];
-  let remainder = num;
-  let nextPrime = getPrime();
 
+  let factors = []; // array to hold results
+  let remainder = num; // initialize remainder with passed in num
+  let nextPrime = getPrime(); // create our prime number generator
+
+  // look at each prime number and see if it is a factor
   do {
+
+    // get the next prime number
     let prime = nextPrime.next().value;
+
+    // is this prime number a factor
     if (remainder % prime === 0) {
+
+      // save factor in answer array
       factors.push(prime)
+
       remainder = remainder / prime;
+
+      // reset our prime number enerator
+
       nextPrime = getPrime();
-      // console.log({prime, factors})
+
     }
-    // console.log(remainder)
+
   } while (remainder > 1)
 
   return factors
 }
 
+// generator function to create prime numbers
 function* getPrime() {
+
+  // intialize values
   let prime = 2;
   let isPrime = true;
 
+  // infinate loop.  Prime numberes are generated without limit
   while (true) {
-    for (let x = 2; x < prime; x++) {
-      if (prime % x === 0) {
+
+    // is prime a prime number
+    for (let factor = 2; factor < prime; factor++) {
+      if (prime % factor === 0) {
         isPrime = false;
         break;
       }
     }
+
+    // if it is a prime number stop the function and return
     if (isPrime) {
       yield prime;
     }
+
+    // go to next number
     prime++;
+
+    // reset isPrime to inital state
     isPrime = true;
   }
 
